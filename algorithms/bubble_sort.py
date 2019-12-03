31b371d38f51d16d7408afe672c46ed5ba322c03
Prashant Singh <prashant_singh@live.in>
2019-12-03 10:07:00 -0600

    alogirthms: optimised bubble-sort for sorted array

--- algorithms/bubble_sort.py
+++ algorithms/bubble_sort.py
@@ -2,8 +2,12 @@ def bubbleSort(arr):
     arrLen = len(arr) - 1
 
     for i in range(arrLen):
+        swapped = False
         for j in range(arrLen - i):
             if arr[j] > arr[j + 1]:
+                swapped = True
                 arr[j], arr[j + 1] = arr[j + 1], arr[j]
-
+        if not swapped:
+            break
+        
     return arr 
